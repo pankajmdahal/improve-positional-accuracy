f8d626cda33fec85e2dc2227e74aa374764ade03
Pankaj <fusion.pankaj@gmail.com>
2020-08-21 12:30:59 -0400

    fixed output plotting based on the output from find_route

--- merge_maps(c2)/plot.py
+++ merge_maps(c2)/plot.py
@@ -1,8 +1,8 @@
 import arcpy
-import pandas
+import pandas as pd
 import csv
 from merge import *
-
+import numpy as np
 
 arcpy.env.overwriteOutput = True
 
@@ -11,26 +11,12 @@ other = "./intermediate/NARN_LINE_03162018.shp"
 other_f = "other_f"
 arcpy.MakeFeatureLayer_management(other, other_f)
 
-
-
-def import_dict_from_csv(filename):
-    data = pandas.read_csv(filename)
-    no_of_columns = len(data.columns)
-    if no_of_columns == 2:
-        datadict = {int(column[0]): [column[1]] for index, column in data.iterrows()}
-    elif no_of_columns == 3:
-        datadict = {int(column[0]): [column[1], column[2]] for index, column in data.iterrows()}
-    else:
-        print("number of columns exceeded")
-        print ("{0} columns ".format(no_of_columns-1))
-        print(filename)
-    return datadict
-
+all_dict = np.load('./intermediate/link_route_type_dict.npy').item()
 
 # read from csv files to a dict
-no_routes_found_dict = import_dict_from_csv(no_routes)
-routes_out_of_buffer = import_dict_from_csv(no_tolerance)
-routes_within_buffer = import_dict_from_csv(no_tolerance_buffer)
+no_routes_found_dict = {x: y for x, y in all_dict.items() if y[2] == 1}
+routes_out_of_buffer = {x: y for x, y in all_dict.items() if y[2] == 2}
+routes_within_buffer = {x: y for x, y in all_dict.items() if y[2] == 3}
 
 
 def get_where_clause(colname, list_of_link_ids):
@@ -48,22 +34,21 @@ arcpy.CopyFeatures_management(other_f, output_no_routes_shp)
 
 # create no_tolerance_within_buffer shp
 route_within_threshold_dict = {x: y for x, y in routes_out_of_buffer.iteritems() if
-                               ((abs(y[0] - y[1]) / y[1]) > float(threshold) / 100)}
+                               ((abs(y[1] - y[0]) / y[0]) > float(threshold) / 100)}
 where_clause = get_where_clause("_ID_", route_within_threshold_dict.keys())
 arcpy.SelectLayerByAttribute_management(other_f, "NEW_SELECTION", where_clause)
 arcpy.CopyFeatures_management(other_f, output_tolerance_exceed_shp)
 arcpy.AddField_management(output_tolerance_exceed_shp, '_RLENG_', "FLOAT")
 arcpy.AddField_management(output_tolerance_exceed_shp, '_TOL_', "FLOAT")
-with arcpy.da.UpdateCursor(output_tolerance_exceed_shp, ['_ID_', '_RLENG_',"_LEN_", "_TOL_"]) as cursor:
+with arcpy.da.UpdateCursor(output_tolerance_exceed_shp, ['_ID_', '_RLENG_', "_LEN_", "_TOL_"]) as cursor:
     for row in cursor:
-        row[1] = route_within_threshold_dict[row[0]][0]
-        row[3] = abs(row[1]-row[2])/row[2]
+        row[1] = route_within_threshold_dict[row[0]][1]
+        row[3] = abs(row[1] - row[2]) / row[2]
         cursor.updateRow(row)
 
 # create no_tolerance_shp
 route_buffer_within_threshold_dict = {x: y for x, y in routes_within_buffer.iteritems() if
-                               ((abs(y[0] - y[1]) / y[1]) > float(threshold) / 100)}
-
+                                      ((abs(y[1] - y[0]) / y[0]) > float(threshold) / 100)}
 
 where_clause = get_where_clause("_ID_", route_buffer_within_threshold_dict.keys())
 arcpy.SelectLayerByAttribute_management(other_f, "NEW_SELECTION", where_clause)
@@ -72,6 +57,6 @@ arcpy.AddField_management(output_buffer_tolerance_exceed_shp, '_RLENG_', "FLOAT"
 arcpy.AddField_management(output_buffer_tolerance_exceed_shp, '_TOL_', "FLOAT")
 with arcpy.da.UpdateCursor(output_buffer_tolerance_exceed_shp, ['_ID_', '_RLENG_', "_LEN_", "_TOL_"]) as cursor:
     for row in cursor:
-        row[1] = route_buffer_within_threshold_dict[row[0]][0]
+        row[1] = route_buffer_within_threshold_dict[row[0]][1]
         row[3] = abs(row[1] - row[2]) / row[2]
         cursor.updateRow(row)
